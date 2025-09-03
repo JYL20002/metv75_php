b18a19d8dabc79cd441bb04aa02a05fe6b338746
焦钰龙 <1033408666@qq.com>
2025-09-03 10:54:24 +0800

    JYL

---
+++ app/system/webset/admin/templates/js/email.js
@@ -0,0 +1,19 @@
+/* 米拓企业建站系统 Copyright (C) 长沙米拓信息技术有限公司 (https://www.metinfo.cn). All rights reserved. */
+;(function() {
+  var that = $.extend(true, {}, admin_module);
+  TEMPLOADFUN[that.hash] = () => {
+    M.ajax({
+      url: that.own_name + '&c=email&a=doGetEmail',
+      success: function(result) {
+        let data = result.data
+        Object.keys(data).map(item => {
+          if (item === 'met_fd_way') {
+            $(`#radio_${data[item]}`).attr('checked', 'checked')
+            return
+          }
+          $(`[name=${item}]`).val(data[item])
+        })
+      }
+    });
+  };
+})()
